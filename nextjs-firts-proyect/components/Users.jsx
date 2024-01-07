9fa55b8b2406e4335130a6884190fc3c59c417a5
patxi <patxiarana05@gmail.com>
2024-01-07 11:27:12 -0300

    Format document

--- nextjs-firts-proyect/components/Users.jsx
+++ nextjs-firts-proyect/components/Users.jsx
@@ -1,28 +1,33 @@
 "use client"
 
+import Link from "next/link";
 
 
-function Users( { users }){
 
-    return(
-        
-<ul>
-{
- users.map(user =>(
-   <li key={user.id} className="bg-slate-400 mb-2 p-4 rounded-r-md flex justify-between" 
-   >
+function Users({ users }) {
 
-     <div>
-       <h5 className="font-bold">{user.id} . {user.first_name} . {user.last_name}</h5>
-       <p className="text-slate-100">email:{user.email}</p>
-     </div>
-     <img src={user.avatar}  className="rounded-full w-20" />
-   </li>
- ))
-}
- </ul>
+    return (
+
+        <ul>
+            {
+                users.map(user => (
+                    <Linkn key={user.id} hfref={`/users/${user.id}`} >
+                        <li className="bg-slate-400 mb-2 p-4 rounded-r-md flex justify-between"
+                            onClick={() => alert(user.id)}
+                        >
+
+                            <div>
+                                <h5 className="font-bold">{user.id} . {user.first_name} . {user.last_name}</h5>
+                                <p className="text-slate-100">email:{user.email}</p>
+                            </div>
+                            <img src={user.avatar} className="rounded-full w-20" />
+                        </li>
+                    </Linkn>
+                ))
+            }
+        </ul>
     )
 }
 
-export default Users; 
+export default Users;
 
